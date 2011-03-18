14389daf04b18f4dd22ac557afb81826795fdc9b
Andrew Wooster <wooster_spam@nextthing.org>
2011-03-18 16:33:46 -0700

    Wee

---
+++ FeedbackDemo/FeedbackDemo/RootViewController.h
@@ -0,0 +1,15 @@
+//
+//  RootViewController.h
+//  FeedbackDemo
+//
+//  Created by Andrew Wooster on 3/18/11.
+//  Copyright 2011 Planetary Scale LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+- (IBAction)showFeedback:(id)sender;
+@end

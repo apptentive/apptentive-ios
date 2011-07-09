10cd48b117daa2b059ec737acd09513a1e02a2f4
Andrew Wooster <wooster_spam@nextthing.org>
2011-07-08 20:22:06 -0600

    Triggering events.

--- ApptentiveConnect/source/Rating Flow/ATAppRatingFlow.m
+++ ApptentiveConnect/source/Rating Flow/ATAppRatingFlow.m
@@ -40,6 +40,10 @@ static ATAppRatingFlow *sharedRatingFlow = nil;
 - (id)initWithAppID:(NSString *)anITunesAppID {
     if ((self = [super init])) {
         iTunesAppID = [anITunesAppID retain];
+        self.daysBeforePrompt = kATAppRatingDefaultDaysBeforePrompt;
+        self.usesBeforePrompt = kATAppRatingDefaultUsesBeforePrompt;
+        self.significantEventsBeforePrompt = kATAppRatingDefaultSignificantEventsBeforePrompt;
+        self.daysBeforeRePrompting = kATAppRatingDefaultDaysBeforeRePrompting;
     }
     return self;
 }
@@ -94,7 +98,81 @@ static ATAppRatingFlow *sharedRatingFlow = nil;
 }
 
 - (BOOL)shouldShowDialog {
-    return NO;//!!
+    BOOL result = NO;
+    
+    do { // once
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        
+        // Check to see if the user has rejected rating this version.
+        NSNumber *rejected = [defaults objectForKey:ATAppRatingFlowDeclinedToRateThisVersionKey];
+        if (rejected != nil && [rejected boolValue]) {
+            break;
+        }
+        
+        // Check to see if the user dislikes this version of the app.
+        NSNumber *dislikes = [defaults objectForKey:ATAppRatingFlowUserDislikesThisVersionKey];
+        if (dislikes != nil && [dislikes boolValue]) {
+            break;
+        }
+        
+        // If we don't have the last version set, update it and don't show
+        // the dialog.
+        NSString *lastBundleVersion = [defaults objectForKey:ATAppRatingFlowLastUsedVersionKey];
+        if (lastBundleVersion == nil) {
+            [self updateVersionInfo];
+            break;
+        }
+        
+        // If the user has been prompted already, make sure we're after the
+        // number of days for them to be re-prompted.
+        NSNumber *lastPrompt = [defaults objectForKey:ATAppRatingFlowLastPromptDateKey];
+        if (lastPrompt != nil && self.daysBeforeRePrompting != 0) {
+            double lastPromptDouble = [lastPrompt doubleValue];
+            double nextPromptDouble = lastPromptDouble + 60*60*24*self.daysBeforeRePrompting;
+            if ([[NSDate now] timeIntervalSince1970] < nextPromptDouble) {
+                break;
+            }
+        }
+        
+        // Make sure the user has been using the app long enough to be bothered.
+        NSNumber *firstUse = [defaults objectForKey:ATAppRatingFlowLastUsedVersionFirstUseDateKey];
+        if (firstUse != nil && self.daysBeforePrompt != 0) {
+            double firstUseDouble = [firstUse doubleValue];
+            double nextPromptDouble = firstUseDouble + 60*60*24*self.daysBeforePrompt;
+            if ([[NSDate now] timeIntervalSince1970] < nextPromptDouble) {
+                break;
+            }
+        }
+        
+        // If the number of significant events is big enough, show a prompt.
+        NSNumber *significantEvents = [defaults objectForKey:ATAppRatingFlowSignificantEventsCountKey];
+        if (significantEvents != nil && self.significantEventsBeforePrompt != 0) {
+            NSUInteger count = [significantEvents unsignedIntegerValue];
+            if (count > self.significantEventsBeforePrompt) {
+                result = YES;
+                break;
+            }
+        }
+        
+        // If the number of app uses is big enough, show a prompt.
+        NSNumber *appUses = [defaults objectForKey:ATAppRatingFlowUseCountKey];
+        if (appUses != nil && self.usesBeforePrompt != 0) {
+            NSUInteger count = [appUses unsignedIntegerValue];
+            if (count > self.usesBeforePrompt) {
+                result = YES;
+                break;
+            }
+        }
+        
+        // Only if both the uses and significant events triggers are set to
+        // 0 (disabled), should we show a dialog based on prompt dates here.
+        if (self.usesBeforePrompt == 0 && self.significantEventsBeforePrompt == 0) {
+            result = YES;
+            break;
+        }
+    } while (NO);
+    
+    return result;
 }
 
 - (void)showDialogIfNecessary {
@@ -112,7 +190,9 @@ static ATAppRatingFlow *sharedRatingFlow = nil;
     if (lastBundleVersion == nil || ![lastBundleVersion isEqualToString:currentBundleVersion]) {
         [defaults setObject:currentBundleVersion forKey:ATAppRatingFlowLastUsedVersionKey];
         NSDate *now = [NSDate date];
-        [defaults setDouble:[now timeIntervalSince1970] forKey:ATAppRatingFlowLastUsedVersionFirstUseDateKey];
+        NSNumber *nowObject = [NSNumber numberWithDouble:[now timeIntervalSince1970]];
+        
+        [defaults setObject:nowObject forKey:ATAppRatingFlowLastUsedVersionFirstUseDateKey];
         [defaults setObject:[NSNumber numberWithBool:NO] forKey:ATAppRatingFlowDeclinedToRateThisVersionKey];
         [defaults setObject:[NSNumber numberWithBool:NO] forKey:ATAppRatingFlowUserDislikesThisVersionKey];
         
@@ -155,6 +235,7 @@ static ATAppRatingFlow *sharedRatingFlow = nil;
 - (void)setRatingDialogWasShown {
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     NSDate *now = [NSDate date];
-    [defaults setDouble:[now timeIntervalSince1970] forKey:ATAppRatingFlowLastPromptDateKey];
+    NSNumber *nowObject = [NSNumber numberWithDouble:[now timeIntervalSince1970]];
+    [defaults setObject:nowObject forKey:ATAppRatingFlowLastPromptDateKey];
 }
 @end

58d5360f7bf7267d45f5cd5b90ed4c3e9dca991c
Andrew Wooster <wooster_spam@nextthing.org>
2011-10-28 14:14:55 -0700

    Shadow views look nicer.

---
+++ ApptentiveConnect/source/Custom Views/ATShadowView.h
@@ -0,0 +1,13 @@
+//
+//  ATShadowView.h
+//  ApptentiveConnect
+//
+//  Created by Andrew Wooster on 10/19/11.
+//  Copyright (c) 2011 Apptentive, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATShadowView : UIView
+
+@end
---
+++ ApptentiveConnect/source/Custom Views/ATShadowView.m
@@ -0,0 +1,31 @@
+//
+//  ATShadowView.m
+//  ApptentiveConnect
+//
+//  Created by Andrew Wooster on 10/19/11.
+//  Copyright (c) 2011 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATShadowView.h"
+
+@implementation ATShadowView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end

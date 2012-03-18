848fe1a6fda6488ecb2953c25b50f83cb8c39066
Andrew Wooster <wooster_spam@nextthing.org>
2012-03-18 12:12:01 -0700

    Start of 3.0 development. Integrating surveys support.

--- ApptentiveConnect/source/ATConnect.h
+++ ApptentiveConnect/source/ATConnect.h
@@ -13,7 +13,7 @@
 #import <Cocoa/Cocoa.h>
 #endif
 
-#define kATConnectVersionString @"0.2.1"
+#define kATConnectVersionString @"0.3.0"
 
 #if TARGET_OS_IPHONE
 #define kATConnectPlatformString @"iOS"

e137971cd2c8c075b0baa31a66e3cf19ada29e8d
Andrew Wooster <wooster@nextthing.org>
2012-10-10 14:19:03 -0700

    Sending messages in Message Center is working.

---
+++ ApptentiveConnect/source/Message Center/ATTextMessageUserCell.h
@@ -0,0 +1,13 @@
+//
+//  ATTextMessageUserCell.h
+//  ApptentiveConnect
+//
+//  Created by Andrew Wooster on 10/9/12.
+//  Copyright (c) 2012 Apptentive, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATTextMessageUserCell : UITableViewCell
+
+@end
---
+++ ApptentiveConnect/source/Message Center/ATTextMessageUserCell.m
@@ -0,0 +1,29 @@
+//
+//  ATTextMessageUserCell.m
+//  ApptentiveConnect
+//
+//  Created by Andrew Wooster on 10/9/12.
+//  Copyright (c) 2012 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATTextMessageUserCell.h"
+
+@implementation ATTextMessageUserCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

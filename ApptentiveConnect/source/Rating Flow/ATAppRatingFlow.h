a10d56aa80c67cae8cd77121019e007c55074b45
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-04-11 20:19:25 -0700

    Adding the ability to set the application display name in the AppRatingFlow

--- ApptentiveConnect/source/Rating Flow/ATAppRatingFlow.h
+++ ApptentiveConnect/source/Rating Flow/ATAppRatingFlow.h
@@ -68,6 +68,14 @@
  will be shown.
  */
 - (void)userDidPerformSignificantEvent:(BOOL)canPromptForRating viewController:(UIViewController *)viewController;
+
+/*!
+ Call whenever you would like to set the application name in the ratings
+ dialog, if no application name is set the bundle display name will be 
+ used as a default value
+ */
+-(void)setAppName:(NSString *)appName;
+
 #elif TARGET_OS_MAC
 /*! 
  Call when the application is done launching. If we should be able to

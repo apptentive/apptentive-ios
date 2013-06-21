993e7630f5df6787b98fd01cb7765c520df94e83
Andrew Wooster <wooster_spam@nextthing.org>
2013-06-20 19:03:37 -0700

    Fixing spacing issues and making title metrics match those set by UIAppearance proxy.

--- ApptentiveConnect/source/Message Center/Custom Views/ATDefaultMessageCenterTitleView.m
+++ ApptentiveConnect/source/Message Center/Custom Views/ATDefaultMessageCenterTitleView.m
@@ -13,11 +13,14 @@
 #import "ATConnect_Private.h"
 #import "ATAppConfigurationUpdater.h"
 
-@implementation ATDefaultMessageCenterTitleView
+@implementation ATDefaultMessageCenterTitleView {
+	BOOL showTagline;
+}
 @synthesize title;
 @synthesize imageView;
 
 - (void)setup {
+	showTagline = [[ATConnect sharedConnection] showTagline];
 	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
 	NSString *titleString = [defaults objectForKey:ATAppConfigurationMessageCenterTitleKey];
 	if (titleString == nil) {
@@ -26,12 +29,16 @@
 	
 	self.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 	//self.backgroundColor = [UIColor clearColor];
-	UIImage *image = [ATBackend imageNamed:@"at_apptentive_icon_small"];
-	imageView = [[UIImageView alloc] initWithImage:image];
-	[self addSubview:imageView];
+	if (showTagline) {
+		UIImage *image = [ATBackend imageNamed:@"at_apptentive_icon_small"];
+		imageView = [[UIImageView alloc] initWithImage:image];
+		[self addSubview:imageView];
+	}
 	title = [[UILabel alloc] initWithFrame:CGRectZero];
 	title.text = titleString;
 	title.font = [UIFont boldSystemFontOfSize:20.];
+	title.minimumFontSize = 10;
+	title.adjustsFontSizeToFitWidth = YES;
 	if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
 		title.textColor = [UIColor whiteColor];
 		title.shadowColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.4];
@@ -45,6 +52,29 @@
 	title.backgroundColor = [UIColor clearColor];
 	title.opaque = NO;
 	title.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+	
+	if ([[UINavigationBar class] respondsToSelector:@selector(appearance)]) {
+		NSDictionary *titleTextAttributes = [[UINavigationBar appearance] titleTextAttributes];
+		UIColor *textColor = (UIColor *)titleTextAttributes[UITextAttributeTextColor];
+		UIColor *shadowColor = (UIColor *)titleTextAttributes[UITextAttributeTextShadowColor];
+		UIFont *font = (UIFont *)titleTextAttributes[UITextAttributeFont];
+		NSValue *shadowOffset = (NSValue *)titleTextAttributes[UITextAttributeTextShadowOffset];
+		
+		if (textColor) {
+			title.textColor = textColor;
+		}
+		if (shadowColor) {
+			title.shadowColor = shadowColor;
+		}
+		if (font) {
+			title.font = [UIFont fontWithName:font.fontName size:20];
+		}
+		if (shadowOffset) {
+			UIOffset offset = [shadowOffset UIOffsetValue];
+			title.shadowOffset = CGSizeMake(offset.horizontal, offset.vertical);
+		}
+	}
+	
 	[self addSubview:title];
 }
 
@@ -68,18 +98,21 @@
 
 - (void)layoutSubviews {
 	CGFloat padding = 4;
-	CGRect imageRect = self.imageView.frame;
+	CGRect imageRect = self.imageView ? self.imageView.frame : CGRectZero;
 	
 	[title sizeToFit];
 	CGFloat titleWidth = title.bounds.size.width;
-	if (titleWidth > self.bounds.size.width) {
-		titleWidth -= imageRect.size.width + padding;
+	CGFloat imageSpace = imageRect.size.width + padding;
+	if (titleWidth > (self.bounds.size.width - imageSpace)) {
+		titleWidth -= imageSpace;
 	}
 	
-	CGFloat titleOriginX = floor(self.bounds.size.width*0.5 - titleWidth*0.5) + imageRect.size.width;
+	CGFloat titleOriginX = floor(self.bounds.size.width*0.5 - titleWidth*0.5 + imageRect.size.width*0.5);
 	imageRect.origin.x = titleOriginX - imageRect.size.width - padding;
 	imageRect.origin.y = floor(self.bounds.size.height*0.5 - imageRect.size.height*0.5);
-	self.imageView.frame = imageRect;
+	if (self.imageView) {
+		self.imageView.frame = imageRect;
+	}
 	
 	CGRect titleRect = self.title.frame;
 	titleRect.origin.x = titleOriginX;

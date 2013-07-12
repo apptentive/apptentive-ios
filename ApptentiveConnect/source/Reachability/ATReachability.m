65c2722f6b93ba91975303caf7d2c47cc67faddd
pkamb <peterkamb@gmail.com>
2013-07-12 13:54:39 -0700

    - When Reachability status changes, we now ATLogInfo the new network type. [#49324867]  - NOT currently adding network type to ATDeviceInfo.
    Based on Apple's Reachability example:
    http://developer.apple.com/library/ios/#samplecode/Reachability/Introduction/Intro.html

--- ApptentiveConnect/source/Reachability/ATReachability.m
+++ ApptentiveConnect/source/Reachability/ATReachability.m
@@ -8,6 +8,13 @@
 
 #import "ATReachability.h"
 #import "ATUtilities.h"
+#import "ATLog.h"
+
+typedef enum {
+	NotReachable = 0,
+	ReachableViaWiFi,
+	ReachableViaWWAN
+} NetworkStatus;
 
 NSString *const ATReachabilityStatusChanged = @"ATReachabilityStatusChanged";
 
@@ -44,8 +51,86 @@ static void ATReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkRea
 	
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 	ATReachability *reachability = (ATReachability *)info;
+	
+	[[ATReachability sharedReachability] updateDeviceInfoWithCurrentNetworkType:reachability];
+	
 	[[NSNotificationCenter defaultCenter] postNotificationName:ATReachabilityStatusChanged object:reachability];
 	[pool release];
+}
+
+- (void)updateDeviceInfoWithCurrentNetworkType:(ATReachability *)reachability {
+	NetworkStatus status = [reachability currentReachabilityStatus];
+	
+	//TODO:
+	//ATDeviceInfo is not currently being updated with the new network type.
+	
+	if(status == NotReachable)
+	{
+		//No internet
+		ATLogInfo(@"Apptentive Reachability changed: no internet");
+	}
+	else if (status == ReachableViaWiFi)
+	{
+		//WiFi
+		ATLogInfo(@"Apptentive Reachability changed: wifi");
+	}
+	else if (status == ReachableViaWWAN)
+	{
+		//Cellular network
+		ATLogInfo(@"Apptentive Reachability changed: WWAN");
+	}
+}
+
+- (NetworkStatus)currentReachabilityStatus
+{
+	NSAssert(reachabilityRef != NULL, @"currentNetworkStatus called with NULL reachabilityRef");
+	NetworkStatus status = NotReachable;
+	SCNetworkReachabilityFlags flags;
+	if (SCNetworkReachabilityGetFlags(reachabilityRef, &flags))
+	{
+		status = [self networkStatusForFlags: flags];
+	}
+	return status;
+}
+
+- (NetworkStatus) networkStatusForFlags: (SCNetworkReachabilityFlags) flags
+{
+	if ((flags & kSCNetworkReachabilityFlagsReachable) == 0)
+	{
+		// if target host is not reachable
+		return NotReachable;
+	}
+	
+	BOOL status = NotReachable;
+	
+	if ((flags & kSCNetworkReachabilityFlagsConnectionRequired) == 0)
+	{
+		// if target host is reachable and no connection is required
+		//  then we'll assume (for now) that you're on Wi-Fi
+		status = ReachableViaWiFi;
+	}
+	
+	if ((((flags & kSCNetworkReachabilityFlagsConnectionOnDemand ) != 0) ||
+		 (flags & kSCNetworkReachabilityFlagsConnectionOnTraffic) != 0))
+	{
+		// ... and the connection is on-demand (or on-traffic) if the
+		//     calling application is using the CFSocketStream or higher APIs
+		
+		if ((flags & kSCNetworkReachabilityFlagsInterventionRequired) == 0)
+		{
+			// ... and no [user] intervention is needed
+			status = ReachableViaWiFi;
+		}
+	}
+	
+	if ((flags & kSCNetworkReachabilityFlagsIsWWAN) == kSCNetworkReachabilityFlagsIsWWAN)
+	{
+		// ... but WWAN connections are OK if the calling application
+		//     is using the CFNetwork (CFSocketStream?) APIs.
+		status = ReachableViaWWAN;
+	}
+	
+	return status;
 }
 
 - (void)dealloc {
@@ -56,7 +141,6 @@ static void ATReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkRea
 	}
 	[super dealloc];
 }
-
 
 - (ATNetworkStatus)currentNetworkStatus {
 	ATNetworkStatus status = ATNetworkNotReachable;

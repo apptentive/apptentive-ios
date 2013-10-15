807e31b368be150c83cefa0746ea51edf4f0f17d
pkamb <peterkamb@gmail.com>
2013-10-14 19:54:22 -0700

    ATinteractionUsageData object

---
+++ ApptentiveConnect/source/Engagement/Model/ATInteractionUsageData.h
@@ -0,0 +1,36 @@
+//
+//  ATInteractionUsageData.h
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 10/14/13.
+//  Copyright (c) 2013 Apptentive, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ATInteraction.h"
+
+@interface ATInteractionUsageData : NSObject
+
+@property (nonatomic, retain) ATInteraction *interaction;
+@property (nonatomic, retain) NSString *codePoint;
+@property (nonatomic, retain) NSNumber *daysSinceInstall;
+@property (nonatomic, retain) NSNumber *daysSinceUpgrade;
+@property (nonatomic, retain) NSString *applicationVersion;
+@property (nonatomic, retain) NSNumber *codePointInvokesTotal;
+@property (nonatomic, retain) NSNumber *codePointInvokesVersion;
+@property (nonatomic, retain) NSNumber *interactionInvokesTotal;
+@property (nonatomic, retain) NSNumber *interactionInvokesVersion;
+
+- (id)initWithInteraction:(ATInteraction *)interaction atCodePoint:(NSString *)codePoint;
++ (ATInteractionUsageData *)usageDataForInteraction:(ATInteraction *)interaction atCodePoint:(NSString *)codePoint;
++ (NSDictionary *)usageDataForInteraction:(ATInteraction *)interaction
+							  atCodePoint:(NSString *)codePoint
+						 daysSinceInstall:(NSNumber *)daysSinceInstall
+						 daysSinceUpgrade:(NSNumber *)daysSinceUpgrade
+					   applicationVersion:(NSString *)applicationVersion
+					codePointInvokesTotal:(NSNumber *)codePointInvokesTotal
+				  codePointInvokesVersion:(NSNumber *)codePointInvokesVersion
+				  interactionInvokesTotal:(NSNumber *)interactionInvokesTotal
+				interactionInvokesVersion:(NSNumber *)interactionInvokesVersion;
+
+@end
---
+++ ApptentiveConnect/source/Engagement/Model/ATInteractionUsageData.m
@@ -0,0 +1,117 @@
+//
+//  ATInteractionUsageData.m
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 10/14/13.
+//  Copyright (c) 2013 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATInteractionUsageData.h"
+#import "ATEngagementBackend.h"
+#import "ATAppRatingFlow_Private.h"
+
+@implementation ATInteractionUsageData
+
+@synthesize daysSinceInstall = _daysSinceInstall;
+@synthesize daysSinceUpgrade = _daysSinceUpgrade;
+@synthesize applicationVersion = _applicationVersion;
+@synthesize codePointInvokesTotal = _codePointInvokesTotal;
+@synthesize codePointInvokesVersion = _codePointInvokesVersion;
+@synthesize interactionInvokesTotal = _interactionInvokesTotal;
+@synthesize interactionInvokesVersion = _interactionInvokesVersion;
+
+- (id)initWithInteraction:(ATInteraction *)interaction atCodePoint:(NSString *)codePoint {
+	if (self = [super init]) {
+		_interaction = interaction;
+		_codePoint = codePoint;
+	}
+	return self;
+}
+
++ (ATInteractionUsageData *)usageDataForInteraction:(ATInteraction *)interaction atCodePoint:(NSString *)codePoint {
+	ATInteractionUsageData *usageData = [[ATInteractionUsageData alloc] initWithInteraction:interaction atCodePoint:codePoint];
+	return [usageData autorelease];
+}
+
++ (ATInteractionUsageData *)usageDataForInteraction:(ATInteraction *)interaction
+							  atCodePoint:(NSString *)codePoint
+						 daysSinceInstall:(NSNumber *)daysSinceInstall
+						 daysSinceUpgrade:(NSNumber *)daysSinceUpgrade
+					   applicationVersion:(NSString *)applicationVersion
+					codePointInvokesTotal:(NSNumber *)codePointInvokesTotal
+				  codePointInvokesVersion:(NSNumber *)codePointInvokesVersion
+				  interactionInvokesTotal:(NSNumber *)interactionInvokesTotal
+				interactionInvokesVersion:(NSNumber *)interactionInvokesVersion
+{
+	ATInteractionUsageData *usageData = [ATInteractionUsageData usageDataForInteraction:interaction atCodePoint:codePoint];
+	usageData.daysSinceInstall = daysSinceInstall;
+	usageData.daysSinceUpgrade = daysSinceUpgrade;
+	usageData.applicationVersion = applicationVersion;
+	usageData.codePointInvokesTotal = codePointInvokesTotal;
+	usageData.codePointInvokesVersion = codePointInvokesVersion;
+	usageData.interactionInvokesTotal = interactionInvokesTotal;
+	usageData.interactionInvokesVersion = interactionInvokesVersion;
+	
+	return usageData;
+}
+
+- (NSNumber *)daysSinceInstall {
+	if (!_daysSinceInstall) {
+		NSDate *installDate = [[NSUserDefaults standardUserDefaults] objectForKey:ATEngagementInstallDateKey];
+		NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+		_daysSinceInstall = [@([[calendar components:NSDayCalendarUnit fromDate:installDate toDate:[NSDate date] options:0] day] + 1) retain];
+		[calendar release];
+	}
+	
+	return _daysSinceInstall;
+}
+
+- (NSNumber *)daysSinceUpgrade {
+	if (!_daysSinceUpgrade) {
+		NSDate *upgradeDate = [[NSUserDefaults standardUserDefaults] objectForKey:ATEngagementUpgradeDateKey];
+		NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+		_daysSinceUpgrade = [@([[calendar components:NSDayCalendarUnit fromDate:upgradeDate toDate:[NSDate date] options:0] day] + 1) retain];
+		[calendar release];
+	}
+		
+	return _daysSinceUpgrade;
+}
+
+- (NSString *)applicationVersion {
+	if (!_applicationVersion) {
+		_applicationVersion = [[[NSUserDefaults standardUserDefaults] objectForKey:ATAppRatingFlowLastUsedVersionKey] retain];
+	}
+	
+	return _applicationVersion;
+}
+
+- (NSNumber *)codePointInvokesTotal {
+	if (!_codePointInvokesTotal) {
+		_codePointInvokesTotal = [[[[NSUserDefaults standardUserDefaults] objectForKey:ATEngagementCodePointsInvokesTotalKey] objectForKey:self.codePoint] retain];
+	}
+	
+	return _codePointInvokesTotal;
+}
+
+- (NSNumber *)codePointInvokesVersion {
+	if (!_codePointInvokesVersion) {
+		_codePointInvokesVersion = [[[[NSUserDefaults standardUserDefaults] objectForKey:ATEngagementCodePointsInvokesVersionKey] objectForKey:self.codePoint] retain];
+	}
+	return _codePointInvokesVersion;
+}
+
+- (NSNumber *)interactionInvokesTotal {
+	if (!_interactionInvokesTotal) {
+		_interactionInvokesTotal = [[[[NSUserDefaults standardUserDefaults] objectForKey:ATEngagementInteractionsInvokesTotalKey] objectForKey:self.interaction.identifier] retain];
+	}
+	return _interactionInvokesTotal;
+}
+
+- (NSNumber *)interactionInvokesVersion {
+	if (!_interactionInvokesVersion) {
+		_interactionInvokesVersion = [[[[NSUserDefaults standardUserDefaults] objectForKey:ATEngagementInteractionsInvokesVersionKey] objectForKey:self.interaction.identifier] retain];
+	}
+	return _interactionInvokesVersion;
+}
+
+@end

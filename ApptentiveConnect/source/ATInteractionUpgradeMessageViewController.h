4749f1ce31116543e013896836c19ee9746e0303
pkamb <peterkamb@gmail.com>
2013-10-16 15:03:11 -0700

    Added ATInteractionUpgradeMessageViewController

---
+++ ApptentiveConnect/source/ATInteractionUpgradeMessageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ATInteractionUpgradeMessageViewController.h
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 10/16/13.
+//  Copyright (c) 2013 Apptentive, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATInteractionUpgradeMessageViewController : UIViewController
+
+@end
---
+++ ApptentiveConnect/source/ATInteractionUpgradeMessageViewController.m
@@ -0,0 +1,38 @@
+//
+//  ATInteractionUpgradeMessageViewController.m
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 10/16/13.
+//  Copyright (c) 2013 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATInteractionUpgradeMessageViewController.h"
+
+@interface ATInteractionUpgradeMessageViewController ()
+
+@end
+
+@implementation ATInteractionUpgradeMessageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

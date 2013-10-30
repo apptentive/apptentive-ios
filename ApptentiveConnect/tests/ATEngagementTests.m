5061cb2f8cbab3a7566dbe4e707a353279aa76c3
pkamb <peterkamb@gmail.com>
2013-10-29 17:47:45 -0700

    Tests for complex criteria

--- ApptentiveConnect/tests/ATEngagementTests.m
+++ ApptentiveConnect/tests/ATEngagementTests.m
@@ -26,7 +26,7 @@
 	ATInteraction *interaction = [[ATInteraction alloc] init];
 	ATInteractionUsageData *usageData;
 	
-	interaction.criteria = @{@"days_since_install" : @{@"$gt" : @5, @"$lt" : @7}};
+	interaction.criteria = @{@"days_since_install": @{@"$gt": @5, @"$lt": @7}};
 	usageData = [ATInteractionUsageData usageDataForInteraction:interaction
 											   daysSinceInstall:@6
 											   daysSinceUpgrade:@6
@@ -43,7 +43,7 @@
 	ATInteraction *interaction = [[ATInteraction alloc] init];
 	ATInteractionUsageData *usageData = [[ATInteractionUsageData alloc] init];
 	
-	interaction.criteria = @{@"days_since_install" : @6};
+	interaction.criteria = @{@"days_since_install": @6};
 	usageData.daysSinceInstall = @6;
 	STAssertTrue([interaction criteriaAreMetForUsageData:usageData], @"Install date");
 	usageData.daysSinceInstall = @5;
@@ -51,7 +51,7 @@
 	usageData.daysSinceInstall = @7;
 	STAssertFalse([interaction criteriaAreMetForUsageData:usageData], @"Install date");
 	
-	interaction.criteria = @{@"days_since_install" : @{@"$gt" : @5, @"$lt" : @7}};
+	interaction.criteria = @{@"days_since_install": @{@"$gt": @5, @"$lt": @7}};
 	usageData.daysSinceInstall = @6;
 	STAssertTrue([interaction criteriaAreMetForUsageData:usageData], @"Install date");
 	usageData.daysSinceInstall = @5;
@@ -59,7 +59,7 @@
 	usageData.daysSinceInstall = @7;
 	STAssertFalse([interaction criteriaAreMetForUsageData:usageData], @"Install date");
 	
-	interaction.criteria = @{@"days_since_install" : @{@"$lte" : @5, @"$gt" : @3}};
+	interaction.criteria = @{@"days_since_install": @{@"$lte": @5, @"$gt": @3}};
 	usageData.daysSinceInstall = @3;
 	STAssertFalse([interaction criteriaAreMetForUsageData:usageData], @"Install date");
 	usageData.daysSinceInstall = @4;
@@ -74,13 +74,13 @@
 	ATInteraction *interaction = [[ATInteraction alloc] init];
 	ATInteractionUsageData *usageData = [[ATInteractionUsageData alloc] init];
 	
-	interaction.criteria = @{@"application_version" : @"1.2.8"};
+	interaction.criteria = @{@"application_version": @"1.2.8"};
 	usageData.applicationVersion = @"1.2.8";
 	STAssertTrue([interaction criteriaAreMetForUsageData:usageData], @"Version number");
 	usageData.applicationVersion = @"v1.2.8";
 	STAssertFalse([interaction criteriaAreMetForUsageData:usageData], @"Version number must not have a 'v' in front!");
 	
-	interaction.criteria = @{@"application_version" : @"v3.0"};
+	interaction.criteria = @{@"application_version": @"v3.0"};
 	usageData.applicationVersion = @"v3.0";
 	STAssertTrue([interaction criteriaAreMetForUsageData:usageData], @"Version number");
 	usageData.applicationVersion = @"3.0";
@@ -100,7 +100,7 @@
 	STAssertFalse([interaction criteriaAreMetForUsageData:usageData], @"Codepoint version invokes.");
 
 	
-	interaction.criteria = @{@"code_point/big.win/invokes/version" : @7};
+	interaction.criteria = @{@"code_point/big.win/invokes/version": @7};
 	usageData.codePointInvokesVersion = @{@"code_point/big.win/invokes/version": @7};
 	STAssertTrue([interaction criteriaAreMetForUsageData:usageData], @"Codepoint version invokes.");
 	usageData.codePointInvokesVersion = @{@"code_point/big.win/invokes/version": @1};
@@ -108,7 +108,7 @@
 	usageData.codePointInvokesVersion = @{@"code_point/big.win/invokes/version": @19};
 	STAssertFalse([interaction criteriaAreMetForUsageData:usageData], @"Codepoint version invokes.");
 
-	interaction.criteria = @{@"code_point/big.win/invokes/version" : @{@"$gte": @5, @"$lte" : @5}};
+	interaction.criteria = @{@"code_point/big.win/invokes/version": @{@"$gte": @5, @"$lte": @5}};
 	usageData.codePointInvokesVersion = @{@"code_point/big.win/invokes/version": @5};
 	STAssertTrue([interaction criteriaAreMetForUsageData:usageData], @"Codepoint version invokes.");
 	usageData.codePointInvokesVersion = @{@"code_point/big.win/invokes/version": @3};
@@ -122,7 +122,7 @@
 	ATInteractionUsageData *usageData = [[ATInteractionUsageData alloc] init];
 	
 	interaction.criteria = @{@"code_point/app.launch/invokes/version": @1,
-							 @"application_version" : @"1.3.0"};
+							 @"application_version": @"1.3.0"};
 	usageData.codePointInvokesVersion = @{@"code_point/app.launch/invokes/version": @1};
 	usageData.applicationVersion = @"1.3.0";
 	STAssertTrue([interaction criteriaAreMetForUsageData:usageData], @"Test Upgrade Message.");
@@ -134,7 +134,7 @@
 	STAssertFalse([interaction criteriaAreMetForUsageData:usageData], @"Test Upgrade Message.");
 
 	interaction.criteria = @{@"application_version": @"1.3.0",
-							 @"code_point/app.launch/invokes/version" : @{@"$gte": @1}};
+							 @"code_point/app.launch/invokes/version": @{@"$gte": @1}};
 	usageData.codePointInvokesVersion = @{@"code_point/app.launch/invokes/version": @1};
 	usageData.applicationVersion = @"1.3.0";
 	STAssertTrue([interaction criteriaAreMetForUsageData:usageData], @"Test Upgrade Message.");
@@ -145,8 +145,8 @@
 	usageData.applicationVersion = @"1.3.0";
 	STAssertFalse([interaction criteriaAreMetForUsageData:usageData], @"Test Upgrade Message.");
 	
-	interaction.criteria = @{@"application_version" : @"1.3.0",
-							 @"code_point/app.launch/invokes/version": @{@"$lte" : @4}};
+	interaction.criteria = @{@"application_version": @"1.3.0",
+							 @"code_point/app.launch/invokes/version": @{@"$lte": @4}};
 	usageData.codePointInvokesVersion = @{@"code_point/app.launch/invokes/version": @1};
 	usageData.applicationVersion = @"1.3.0";
 	STAssertTrue([interaction criteriaAreMetForUsageData:usageData], @"Test Upgrade Message.");
@@ -193,7 +193,6 @@
 																 options:NSJSONReadingAllowFragments
 																   error:nil];
 	
-	
 	NSDictionary *codePoints = [interactions objectForKey:@"interactions"];
 	NSDictionary *complexInteractionDictionary = [[codePoints objectForKey:@"complext_criteria"] objectAtIndex:0];
 	
@@ -207,9 +206,27 @@
 	
 	usageData.daysSinceUpgrade = @3;
 	usageData.codePointInvokesTotal = @{@"code_point/app.launch/invokes/total": @8};
-	STAssertFalse([complexInteraction criteriaAreMetForUsageData:usageData], @"3 fails the initial OR clause.");
+	STAssertFalse([complexInteraction criteriaAreMetForUsageData:usageData], @"3 fails the initial OR clause. 8 fails the other clause.");
 
-
+	usageData.daysSinceUpgrade = @3;
+	usageData.interactionInvokesVersion = @{@"interactions/526fe2836dd8bf546a00000b/invokes/version": @0};
+	usageData.codePointInvokesTotal = @{@"code_point/app.launch/invokes/total": @2,
+										@"code_point/small.win/invokes/total": @0,
+										@"code_point/big.win/invokes/total": @2};
+	STAssertTrue([complexInteraction criteriaAreMetForUsageData:usageData], @"complex");
+	usageData.codePointInvokesTotal = @{@"code_point/app.launch/invokes/total": @2,
+										@"code_point/small.win/invokes/total": @2,
+										@"code_point/big.win/invokes/total": @19};
+	STAssertTrue([complexInteraction criteriaAreMetForUsageData:usageData], @"complex");
+	usageData.codePointInvokesTotal = @{@"code_point/app.launch/invokes/total": @2,
+										@"code_point/small.win/invokes/total": @19,
+										@"code_point/big.win/invokes/total": @19};
+	STAssertFalse([complexInteraction criteriaAreMetForUsageData:usageData], @"Neither of the last two ORed code_point totals are right.");
+	usageData.codePointInvokesTotal = @{@"code_point/app.launch/invokes/total": @2,
+										@"code_point/small.win/invokes/total": @2,
+										@"code_point/big.win/invokes/total": @1};
+	usageData.interactionInvokesVersion = @{@"interactions/526fe2836dd8bf546a00000b/invokes/version": @8};
+	STAssertFalse([complexInteraction criteriaAreMetForUsageData:usageData], @"The middle case is incorrect.");
 }
 
 @end

5af738632aea4b7f5fe82fd807394bebbaaa26c8
Andrew Wooster <wooster_spam@nextthing.org>
2013-11-04 16:37:29 -0800

    Removing alpha key.

--- ApptentiveConnect/source/ATConnect.h
+++ ApptentiveConnect/source/ATConnect.h
@@ -13,7 +13,7 @@
 #import <Cocoa/Cocoa.h>
 #endif
 
-#define kATConnectVersionString @"1.2.2a"
+#define kATConnectVersionString @"1.2.2"
 
 #if TARGET_OS_IPHONE
 #	define kATConnectPlatformString @"iOS"

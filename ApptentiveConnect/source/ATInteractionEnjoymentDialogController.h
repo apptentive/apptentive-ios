57c0553dcfbb6de150e1a1982ebddeef709aeea2
pkamb <peterkamb@gmail.com>
2014-02-18 15:52:10 -0800

    Added `ATInteractionEnjoymentDialogController`

---
+++ ApptentiveConnect/source/ATInteractionEnjoymentDialogController.h
@@ -0,0 +1,13 @@
+//
+//  ATInteractionEnjoymentDialogController.h
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 2/18/14.
+//  Copyright (c) 2014 Apptentive, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATInteractionEnjoymentDialogController : NSObject
+
+@end
---
+++ ApptentiveConnect/source/ATInteractionEnjoymentDialogController.m
@@ -0,0 +1,13 @@
+//
+//  ATInteractionEnjoymentDialogController.m
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 2/18/14.
+//  Copyright (c) 2014 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATInteractionEnjoymentDialogController.h"
+
+@implementation ATInteractionEnjoymentDialogController
+
+@end

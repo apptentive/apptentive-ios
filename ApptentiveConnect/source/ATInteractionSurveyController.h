9499ec4e0a683b05b4aaca59c2cb9119d25883e7
pkamb <peterkamb@gmail.com>
2014-04-09 16:01:38 -0700

    Surveys Controller for interaction-based surveys IOS-463

---
+++ ApptentiveConnect/source/ATInteractionSurveyController.h
@@ -0,0 +1,13 @@
+//
+//  ATInteractionSurveyController.h
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 4/9/14.
+//  Copyright (c) 2014 Apptentive, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATInteractionSurveyController : NSObject
+
+@end
---
+++ ApptentiveConnect/source/ATInteractionSurveyController.m
@@ -0,0 +1,13 @@
+//
+//  ATInteractionSurveyController.m
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 4/9/14.
+//  Copyright (c) 2014 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATInteractionSurveyController.h"
+
+@implementation ATInteractionSurveyController
+
+@end

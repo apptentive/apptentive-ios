351f2de07e04499fa9e89b76650c17b6acc4b41f
Andrew Wooster <wooster_spam@nextthing.org>
2014-04-10 18:32:26 -0700

    1.4.0 -> 1.4.1

--- ApptentiveConnect/source/ATConnect.h
+++ ApptentiveConnect/source/ATConnect.h
@@ -13,7 +13,7 @@
 #import <Cocoa/Cocoa.h>
 #endif
 
-#define kATConnectVersionString @"1.4.0"
+#define kATConnectVersionString @"1.4.1"
 
 #if TARGET_OS_IPHONE
 #	define kATConnectPlatformString @"iOS"

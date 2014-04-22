1d5c3bb0cda485f64d1aa406216e4129e561ca59
pkamb <peterkamb@gmail.com>
2014-04-22 12:10:47 -0700

    Removed debug code that added test Survey to interaction manifest.

--- ApptentiveConnect/source/Engagement/Model/ATEngagementManifestParser.m
+++ ApptentiveConnect/source/Engagement/Model/ATEngagementManifestParser.m
@@ -27,13 +27,6 @@
 			NSDictionary *jsonManifest = (NSDictionary *)decodedObject;
 			NSDictionary *jsonCodePoints = [jsonManifest objectForKey:@"interactions"];
 			
-#warning REMOVE; just for testing survey interaction.
-#if APPTENTIVE_DEBUG
-			NSMutableDictionary *added = [[jsonCodePoints mutableCopy] autorelease];
-			[added setObject:@[[self surveyInteractionExample]] forKey:@"local#app#presentSurvey"];
-			jsonCodePoints = added;
-#endif
-			
 			NSMutableDictionary *codePoints = [[NSMutableDictionary alloc] init];
 			for (NSString *codePointName in [jsonCodePoints allKeys]) {
 				NSArray *jsonInteractions = [jsonCodePoints objectForKey:codePointName];
@@ -68,47 +61,6 @@
 - (void)dealloc {
 	[parserError release], parserError = nil;
 	[super dealloc];
-}
-
-#warning REMOVE
-- (NSDictionary *)surveyInteractionExample {
-	NSDictionary *interaction = @{@"id": @"526fe2836dd8bf546a00000c",
-								  @"type": @"Survey",
-								  @"priority": @3,
-								  @"criteria": @{},
-								  @"configuration": @{
-										  @"name": @"What should we build?",
-										  @"show_success_message": @YES,
-										  @"success_message": @"Thank you for your input.",
-										  @"description": @"Please help us figure this out!",
-										  @"questions": @[
-												  @{
-													  @"id": @"52db59c27724c591ab00003e",
-													  @"instructions": @"select one",
-													  @"value": @"Which would you like to see first?",
-													  @"type": @"multichoice",
-													  @"required": @YES,
-													  @"answer_choices": @[
-															  @{
-																  @"id": @"52db59c27724c591ab00003f",
-																  @"value": @"Better user interface"
-															  },
-															  @{
-																  @"id": @"52db59c27724c591ab000040",
-																  @"value": @"Cloud support"
-															  },
-															  @{
-																  @"id": @"52db59c27724c591ab000041",
-																  @"value": @"Login with Facebook / Google / Twitter"
-															  }
-															  ]
-													}
-												  
-												  ]
-										  }
-								  };
-	
-	return interaction;
 }
 
 @end

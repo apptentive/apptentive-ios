ca620b41cb2228e8b1362175d0272392f5f28774
pkamb <peterkamb@gmail.com>
2014-06-10 15:39:30 -0700

    Updated version to 1.5.1

--- ApptentiveConnect/source/ATConnect.h
+++ ApptentiveConnect/source/ATConnect.h
@@ -13,7 +13,7 @@
 #import <Cocoa/Cocoa.h>
 #endif
 
-#define kATConnectVersionString @"1.5.0"
+#define kATConnectVersionString @"1.5.1"
 
 #if TARGET_OS_IPHONE
 #	define kATConnectPlatformString @"iOS"

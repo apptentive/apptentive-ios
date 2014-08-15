e62cf2b575af7f7364c4121c7f3ef921f1ae8cde
pkamb <peterkamb@gmail.com>
2014-08-15 01:50:59 -0700

    Message Center chat arrow now aims down rather than up. Fixes IOS-602

--- ApptentiveConnect/source/Message Center/ATMessageBubbleArrowViewV7.m
+++ ApptentiveConnect/source/Message Center/ATMessageBubbleArrowViewV7.m
@@ -33,25 +33,21 @@
 - (void)drawRect:(CGRect)rect {
 	CGContextRef context = UIGraphicsGetCurrentContext();
 	CGContextSaveGState(context);
-	CGContextScaleCTM(context, 0.5, 0.5);
-	UIBezierPath* bezierPath = [UIBezierPath bezierPath];
-	[bezierPath moveToPoint: CGPointMake(0, 22)];
-	[bezierPath addCurveToPoint: CGPointMake(11, 22) controlPoint1: CGPointMake(0.49, 21.81) controlPoint2: CGPointMake(10.29, 21.98)];
-	[bezierPath addCurveToPoint: CGPointMake(13, 22) controlPoint1: CGPointMake(12.06, 22.04) controlPoint2: CGPointMake(13, 22)];
-	[bezierPath addCurveToPoint: CGPointMake(17, 21) controlPoint1: CGPointMake(14.24, 21.93) controlPoint2: CGPointMake(16.47, 21.14)];
-	[bezierPath addCurveToPoint: CGPointMake(22.49, 18.5) controlPoint1: CGPointMake(18.75, 20.54) controlPoint2: CGPointMake(21.99, 18.76)];
-	[bezierPath addCurveToPoint: CGPointMake(30.99, 1) controlPoint1: CGPointMake(32.12, 13.45) controlPoint2: CGPointMake(30.99, 1)];
-	[bezierPath addCurveToPoint: CGPointMake(16.99, 8) controlPoint1: CGPointMake(30.99, 1) controlPoint2: CGPointMake(22.36, 7.47)];
-	[bezierPath addCurveToPoint: CGPointMake(10, 9) controlPoint1: CGPointMake(13.8, 8.31) controlPoint2: CGPointMake(11.31, 9.06)];
-	[bezierPath addCurveToPoint: CGPointMake(0, 9) controlPoint1: CGPointMake(9.57, 8.98) controlPoint2: CGPointMake(0, 9)];
-	[bezierPath addCurveToPoint: CGPointMake(0, 14) controlPoint1: CGPointMake(0, 9) controlPoint2: CGPointMake(0.1, 12.86)];
-	[bezierPath addCurveToPoint: CGPointMake(0, 22) controlPoint1: CGPointMake(-0.11, 15.23) controlPoint2: CGPointMake(0, 22)];
+	CGContextScaleCTM(context, 1.0, 1.0);
+	UIBezierPath *bezierPath = [UIBezierPath bezierPath];
+	
+	[bezierPath moveToPoint:CGPointMake(9.5, 17)];
+	[bezierPath addCurveToPoint:CGPointMake(14.5, 12) controlPoint1:CGPointMake(13.4, 14.8) controlPoint2:CGPointMake(14.5, 12)];
+	[bezierPath addCurveToPoint:CGPointMake(6, 0) controlPoint1:CGPointMake(14.5, 12) controlPoint2:CGPointMake(9, 0)];
+	[bezierPath addCurveToPoint:CGPointMake(5, 6) controlPoint1:CGPointMake(5.3000002, 0) controlPoint2:CGPointMake(5, 3.5)];
+	[bezierPath addCurveToPoint:CGPointMake(0, 18) controlPoint1:CGPointMake(5, 14.5) controlPoint2:CGPointMake(0, 18)];
+	[bezierPath addCurveToPoint:CGPointMake(9.5, 17) controlPoint1:CGPointMake(0, 18) controlPoint2:CGPointMake(5.5999999, 19.200001)];
 	[bezierPath closePath];
-	if (_direction == ATMessageBubbleArrowDirectionLeft) {
+	if (_direction == ATMessageBubbleArrowDirectionRight) {
 		[bezierPath applyTransform:CGAffineTransformMakeScale(-1, 1)];
-		[bezierPath applyTransform:CGAffineTransformMakeTranslation(39, 0)];
+		[bezierPath applyTransform:CGAffineTransformMakeTranslation(15, -7.5)];
 	} else {
-		[bezierPath applyTransform:CGAffineTransformMakeTranslation(1, 0)];
+		[bezierPath applyTransform:CGAffineTransformMakeTranslation(6, -7.5)];
 	}
 	[self.color setFill];
 	[bezierPath fill];

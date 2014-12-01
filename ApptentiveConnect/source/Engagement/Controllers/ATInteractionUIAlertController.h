ce9704e3afb4f6d1b3c9a27edfeb488f03ef93ba
pkamb <peterkamb@gmail.com>
2014-12-01 15:47:06 -0800

    Added `ATInteractionUIAlertController` Fixes IOS-688

---
+++ ApptentiveConnect/source/Engagement/Controllers/ATInteractionUIAlertController.h
@@ -0,0 +1,13 @@
+//
+//  ATInteractionUIAlertController.h
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 12/1/14.
+//  Copyright (c) 2014 Apptentive, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATInteractionUIAlertController : UIAlertController
+
+@end
---
+++ ApptentiveConnect/source/Engagement/Controllers/ATInteractionUIAlertController.m
@@ -0,0 +1,37 @@
+//
+//  ATInteractionUIAlertController.m
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 12/1/14.
+//  Copyright (c) 2014 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATInteractionUIAlertController.h"
+
+@interface ATInteractionUIAlertController ()
+
+@end
+
+@implementation ATInteractionUIAlertController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

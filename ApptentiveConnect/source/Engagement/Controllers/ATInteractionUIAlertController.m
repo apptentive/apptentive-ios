f6b94664f5f323795daf28221be4ae837f0f1856
pkamb <peterkamb@gmail.com>
2014-12-03 17:01:42 -0800

    Set remote UIAlertController's button action block from the interaction config Fixes IOS-691

--- ApptentiveConnect/source/Engagement/Controllers/ATInteractionUIAlertController.m
+++ ApptentiveConnect/source/Engagement/Controllers/ATInteractionUIAlertController.m
@@ -14,6 +14,8 @@
 
 @implementation ATInteractionUIAlertController
 
+typedef void (^alertActionHandler)(UIAlertAction *);
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
@@ -28,8 +30,8 @@
 	NSDictionary *config = interaction.configuration;
 	NSString *title = config[@"title"];
 	NSString *message = config[@"message"];
-	NSString *style = config[@"style"];
 	
+	NSString *style = config[@"style"];
 	UIAlertControllerStyle preferredStyle;
 	if ([style isEqualToString:@"alert"]) {
 		preferredStyle = UIAlertControllerStyleAlert;
@@ -44,8 +46,8 @@
 	NSArray *actions = config[@"actions"];
 	for (NSDictionary *action in actions) {
 		NSString *actionTitle = action[@"title"];
-		NSString *actionStyle = action[@"style"];
 		
+		NSString *actionStyle = action[@"style"];
 		UIAlertActionStyle alertActionStyle;
 		if ([actionStyle isEqualToString:@"default"]) {
 			alertActionStyle = UIAlertActionStyleDefault;
@@ -56,8 +58,24 @@
 		} else {
 			alertActionStyle = UIAlertActionStyleDefault;
 		}
+
+		NSString *actionType = action[@"type"];
+		alertActionHandler actionHandler;
+		if ([actionType isEqualToString:@"deepLink"]) {
+			NSURL *url = [NSURL URLWithString:@"TODO"];
+			actionHandler = [self createButtonHandlerBlockDeepLink:url];
+		} else if ([actionType isEqualToString:@"engageEvent"]) {
+			NSString *event = @"TODO";
+			actionHandler = [self createButtonHandlerBlockEngage:event];
+		} else if ([actionType isEqualToString:@"dismiss"]) {
+			actionHandler = [self createButtonHandlerBlockDismiss];
+		} else {
+			actionHandler = [self createButtonHandlerBlockDismiss];
+		}
 		
-		UIAlertAction *alertAction = [UIAlertAction actionWithTitle:actionTitle style:alertActionStyle handler:nil /*TODO install a handler*/];
+		UIAlertAction *alertAction = [UIAlertAction actionWithTitle:actionTitle style:alertActionStyle handler:actionHandler];
+		
+		Block_release(actionHandler);
 		
 		BOOL enabled = action[@"enabled"] ? [action[@"enabled"] boolValue] : YES;
 		alertAction.enabled = enabled;
@@ -67,6 +85,24 @@
 	}
 	
 	return alertController;
+}
+
++ (alertActionHandler)createButtonHandlerBlockDismiss {
+	return Block_copy(^(UIAlertAction *action) {
+		ATLogInfo(@"Tapped `Dismiss` button!");
+	});
+}
+
++ (alertActionHandler)createButtonHandlerBlockEngage:(NSString *)event {
+	return Block_copy(^(UIAlertAction *action) {
+		ATLogInfo(@"Tapped `Engage Event` button with event: `%@`", event);
+	});
+}
+
++ (alertActionHandler)createButtonHandlerBlockDeepLink:(NSURL *)url {
+	return Block_copy(^(UIAlertAction *action) {
+		ATLogInfo(@"Tapped `Deep Link` button with URL: `%@`", url);
+	});
 }
 
 @end

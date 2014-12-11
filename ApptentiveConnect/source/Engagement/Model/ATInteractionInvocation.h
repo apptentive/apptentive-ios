0c44b9bc4fa1cf11e25d5a67804e5d4e0af62d52
pkamb <peterkamb@gmail.com>
2014-12-11 15:10:09 -0800

    Added `ATInteractionInvocation` class

---
+++ ApptentiveConnect/source/Engagement/Model/ATInteractionInvocation.h
@@ -0,0 +1,13 @@
+//
+//  ATInteractionInvocation.h
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 12/10/14.
+//  Copyright (c) 2014 Apptentive, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATInteractionInvocation : NSObject
+
+@end
---
+++ ApptentiveConnect/source/Engagement/Model/ATInteractionInvocation.m
@@ -0,0 +1,13 @@
+//
+//  ATInteractionInvocation.m
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 12/10/14.
+//  Copyright (c) 2014 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATInteractionInvocation.h"
+
+@implementation ATInteractionInvocation
+
+@end

2e2a8effbe4d6fb9635ff97ee034b407ee99a0e9
pkamb <peterkamb@gmail.com>
2014-12-19 13:35:11 -0800

    Added `NavigateToLink` interaction type

---
+++ ApptentiveConnect/source/Engagement/Controllers/ATInteractionNavigateToLink.h
@@ -0,0 +1,13 @@
+//
+//  ATInteractionNavigateToLink.h
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 12/19/14.
+//  Copyright (c) 2014 Apptentive, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATInteractionNavigateToLink : NSObject
+
+@end
---
+++ ApptentiveConnect/source/Engagement/Controllers/ATInteractionNavigateToLink.m
@@ -0,0 +1,13 @@
+//
+//  ATInteractionNavigateToLink.m
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 12/19/14.
+//  Copyright (c) 2014 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATInteractionNavigateToLink.h"
+
+@implementation ATInteractionNavigateToLink
+
+@end

d76ad56a0f7d978997daab53849a217607a0a438
pkamb <peterkamb@gmail.com>
2015-01-26 14:57:47 -0800

    Registered Feedback Demo for URL scheme "FeedbackDemo://" Opening a "feedbackdemo://" URL (from a Note, for example) now logs a message in Feedback Demo. This is an example of Deep Linking within the same app on which the Note is presented. IOSOSX-14

--- FeedbackDemo/FeedbackDemo/FeedbackDemoAppDelegate.m
+++ FeedbackDemo/FeedbackDemo/FeedbackDemoAppDelegate.m
@@ -44,6 +44,12 @@
 	return YES;
 }
 
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
+	NSLog(@"Apptentive Feedback Demo handled opening the URL: %@", url);
+	
+	return YES;
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application {
 	/*
 	 Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.

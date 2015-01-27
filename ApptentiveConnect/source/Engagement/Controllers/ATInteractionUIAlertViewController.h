5e72c557455f631b69725bb7b1c0cf634f3ad431
pkamb <peterkamb@gmail.com>
2015-01-26 17:30:05 -0800

    Added `ATInteractionUIAlertViewController`

---
+++ ApptentiveConnect/source/Engagement/Controllers/ATInteractionUIAlertViewController.h
@@ -0,0 +1,13 @@
+//
+//  ATInteractionUIAlertViewController.h
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 1/26/15.
+//  Copyright (c) 2015 Apptentive, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATInteractionUIAlertViewController : NSObject
+
+@end
---
+++ ApptentiveConnect/source/Engagement/Controllers/ATInteractionUIAlertViewController.m
@@ -0,0 +1,13 @@
+//
+//  ATInteractionUIAlertViewController.m
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 1/26/15.
+//  Copyright (c) 2015 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATInteractionUIAlertViewController.h"
+
+@implementation ATInteractionUIAlertViewController
+
+@end

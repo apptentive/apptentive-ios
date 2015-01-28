9a6760d28c2a0b5930c40eb6386a4d33ddcc358d
pkamb <peterkamb@gmail.com>
2015-01-27 17:10:41 -0800

    Combined class for TextModal interactions Supports both `UIAlertView` and `UIAlertController`

---
+++ ApptentiveConnect/source/Engagement/Controllers/ATInteractionTextModalController.h
@@ -0,0 +1,13 @@
+//
+//  ATInteractionTextModalController.h
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 1/27/15.
+//  Copyright (c) 2015 Apptentive, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATInteractionTextModalController : NSObject
+
+@end
---
+++ ApptentiveConnect/source/Engagement/Controllers/ATInteractionTextModalController.m
@@ -0,0 +1,13 @@
+//
+//  ATInteractionTextModalController.m
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 1/27/15.
+//  Copyright (c) 2015 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATInteractionTextModalController.h"
+
+@implementation ATInteractionTextModalController
+
+@end

93af65cbb5886dd583f9bf85d91126445d338c21
pkamb <peterkamb@gmail.com>
2015-01-30 16:39:10 -0800

    Adds `navigate` event to `NavigateToLink` interaction. Adds `url` and `success` keys to `data`.

--- ApptentiveConnect/source/Engagement/Controllers/ATInteractionNavigateToLink.m
+++ ApptentiveConnect/source/Engagement/Controllers/ATInteractionNavigateToLink.m
@@ -8,21 +8,30 @@
 
 #import "ATInteractionNavigateToLink.h"
 
+NSString *const ATInteractionNavigateToLinkEventLabelNavigate = @"navigate";
+
 @implementation ATInteractionNavigateToLink
 
 + (void)navigateToLinkWithInteraction:(ATInteraction *)interaction {
 	NSAssert([interaction.type isEqualToString:@"NavigateToLink"], @"Attempted to load a NavigateToLink interaction with an interaction of type: %@", interaction.type);
 
+	BOOL openedURL = NO;
 	NSURL *url = [NSURL URLWithString:interaction.configuration[@"url"]];
 	if (url) {
 		if ([[UIApplication sharedApplication] canOpenURL:url]) {
-			[[UIApplication sharedApplication] openURL:url];
+			openedURL = [[UIApplication sharedApplication] openURL:url];
 		} else {
 			ATLogError(@"No application can open the Interaction's URL: %@", url);
 		}
 	} else {
 		ATLogError(@"No URL was included in the NavigateToLink Interaction's configuration.");
 	}
+	
+	NSDictionary *userInfo = @{@"url": (url ?: [NSNull null]),
+							   @"success": @(openedURL),
+							   };
+	
+	[interaction engage:ATInteractionNavigateToLinkEventLabelNavigate fromViewController:nil userInfo:userInfo];
 }
 
 @end

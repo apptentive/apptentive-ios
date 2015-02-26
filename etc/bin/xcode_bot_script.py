2a8816e07eec2cfc4680c76c1c5e080a78f149b4
pkamb <peterkamb@gmail.com>
2015-02-26 13:51:32 -0800

    Add displaying integration status on GitHub

--- etc/bin/xcode_bot_script.py
+++ etc/bin/xcode_bot_script.py
@@ -1,2 +1,18 @@
 # This script should be copied into the Run Script trigger of an Xcode Bot
 # `Xcode Bot > Edit Bot > Triggers > After Integration > Run Script`
+
+# Utilizes `cavejohnson` for various integrations
+# https://github.com/drewcrawford/CaveJohnson
+
+#!/bin/bash
+PATH=/Library/Frameworks/Python.framework/Versions/3.4/bin:$PATH
+
+# GitHub
+
+# First set a github auth token like so:
+# sudo -u _xcsbuildd cavejohnson setGithubAuthToken --token auth_token_generated_by_github
+
+# Set build status on GitHub
+cavejohnson setGithubStatus
+
+echo "Finished running Xcode Bot's Run Script Trigger"

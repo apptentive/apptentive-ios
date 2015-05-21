43ebd10133a1c1b32d3ae00893df330cecf27883
Peter Kamb <peterkamb@gmail.com>
2015-05-20 17:40:39 -0700

    Add new `ATMessageCenterViewController`, subclass of `UICollectionViewController`

---
+++ ApptentiveConnect/source/Message Center/ATMessageCenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  ATMessageCenterViewController.h
+//  ApptentiveConnect
+//
+//  Created by Frank Schmitt on 5/20/15.
+//  Copyright (c) 2015 Apptentive, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATMessageCenterViewController : UICollectionViewController
+
+@end

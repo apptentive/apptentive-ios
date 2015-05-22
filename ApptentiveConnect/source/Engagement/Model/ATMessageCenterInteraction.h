b5ec379c42ab8093b0eca7de016cc7744efb19a5
Peter Kamb <peterkamb@gmail.com>
2015-05-22 12:15:45 -0700

    Add ATMessageCenterInteraction subclass of ATInteraction.

---
+++ ApptentiveConnect/source/Engagement/Model/ATMessageCenterInteraction.h
@@ -0,0 +1,13 @@
+//
+//  ATMessageCenterInteraction.h
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 5/22/15.
+//  Copyright (c) 2015 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATInteraction.h"
+
+@interface ATMessageCenterInteraction : ATInteraction
+
+@end
---
+++ ApptentiveConnect/source/Engagement/Model/ATMessageCenterInteraction.m
@@ -0,0 +1,13 @@
+//
+//  ATMessageCenterInteraction.m
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 5/22/15.
+//  Copyright (c) 2015 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATMessageCenterInteraction.h"
+
+@implementation ATMessageCenterInteraction
+
+@end

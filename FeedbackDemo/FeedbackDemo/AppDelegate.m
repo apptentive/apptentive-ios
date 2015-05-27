2a692c005fae51f26d8cb6f88e23ee1f8618dcb8
Frank Schmitt <frank@apptentive.com>
2015-05-27 13:44:51 -0700

    Fixing location of API key and App ID

--- FeedbackDemo/FeedbackDemo/AppDelegate.m
+++ FeedbackDemo/FeedbackDemo/AppDelegate.m
@@ -32,9 +32,9 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application {
 	// Warn developer if API key or app ID are not set.
 	if ([[ATConnect sharedConnection].apiKey isEqualToString:@"ApptentiveApiKey"]) {
-		[[[UIAlertView alloc] initWithTitle:@"Please Set API Key" message:@"This demo app will not work properly until you set your API key in defines.h" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil] show];
+		[[[UIAlertView alloc] initWithTitle:@"Please Set API Key" message:@"This demo app will not work properly until you set your API key in AppDelegate.m" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil] show];
 	} else if ([[ATConnect sharedConnection].appID isEqualToString:@"ExampleAppID"]) {
-		[[[UIAlertView alloc] initWithTitle:@"Please Set App ID" message:@"This demo app won't be able to show your app in the app store until you set your App ID in defines.h" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil] show];
+		[[[UIAlertView alloc] initWithTitle:@"Please Set App ID" message:@"This demo app won't be able to show your app in the app store until you set your App ID in AppDelegate.m" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil] show];
 	}
 }
 

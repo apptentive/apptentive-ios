bd5402f816ebc58144ef9e233ce175494903b846
Peter Kamb <peterkamb@gmail.com>
2015-05-28 08:40:16 -0700

    Add new `ATMessageInputViewController` class + xib.

---
+++ ApptentiveConnect/source/Message Center/ATMessageInputViewController.h
@@ -0,0 +1,13 @@
+//
+//  ATMessageInputViewController.h
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 5/28/15.
+//  Copyright (c) 2015 Apptentive, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATMessageInputViewController : UIViewController
+
+@end

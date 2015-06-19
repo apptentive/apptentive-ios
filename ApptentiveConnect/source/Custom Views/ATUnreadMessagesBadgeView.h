74f7078dc6054d397340647d4b1096b2af7321bd
Peter Kamb <peterkamb@gmail.com>
2015-06-19 14:26:05 -0700

    Add `ATUnreadMessagesBadgeView`

---
+++ ApptentiveConnect/source/Custom Views/ATUnreadMessagesBadgeView.h
@@ -0,0 +1,13 @@
+//
+//  ATUnreadMessagesBadgeView.h
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 6/19/15.
+//  Copyright (c) 2015 Apptentive, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATUnreadMessagesBadgeView : UIView
+
+@end
---
+++ ApptentiveConnect/source/Custom Views/ATUnreadMessagesBadgeView.m
@@ -0,0 +1,13 @@
+//
+//  ATUnreadMessagesBadgeView.m
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 6/19/15.
+//  Copyright (c) 2015 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATUnreadMessagesBadgeView.h"
+
+@implementation ATUnreadMessagesBadgeView
+
+@end

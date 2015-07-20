a9c5d7b9a39bc60d5f758e50466bdff822aa273b
Frank Schmitt <frank@apptentive.com>
2015-07-20 14:26:20 -0700

    Initial UI + landscape

---
+++ ApptentiveConnect/source/Custom Views/ATMessageCenterWhoView.h
@@ -0,0 +1,13 @@
+//
+//  ATMessageCenterWhoView.h
+//  ApptentiveConnect
+//
+//  Created by Frank Schmitt on 7/20/15.
+//  Copyright (c) 2015 Apptentive, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATMessageCenterWhoView : UIView
+
+@end
---
+++ ApptentiveConnect/source/Custom Views/ATMessageCenterWhoView.m
@@ -0,0 +1,21 @@
+//
+//  ATMessageCenterWhoView.m
+//  ApptentiveConnect
+//
+//  Created by Frank Schmitt on 7/20/15.
+//  Copyright (c) 2015 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATMessageCenterWhoView.h"
+
+@implementation ATMessageCenterWhoView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

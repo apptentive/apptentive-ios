a0ec576de12a8d9438f67b0a2daf127fdbf6e699
Peter Kamb <peterkamb@gmail.com>
2015-07-22 11:57:31 -0700

    Add ATMessageCenterContextMessageCell

---
+++ ApptentiveConnect/source/Custom Views/ATMessageCenterContextMessageCell.h
@@ -0,0 +1,13 @@
+//
+//  ATMessageCenterContextMessageCell.h
+//  ApptentiveConnect
+//
+//  Created by Peter Kamb on 7/22/15.
+//  Copyright (c) 2015 Apptentive, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATMessageCenterContextMessageCell : UITableViewCell
+
+@end

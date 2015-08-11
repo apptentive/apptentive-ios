2d5f364fa45210e6d8e0470b6435d9d1ae17fe32
Peter Kamb <peterkamb@gmail.com>
2015-08-10 18:00:42 -0700

    Change warning for missing API Key and App ID from UIAlert to NSLog.

--- FeedbackDemo/FeedbackDemo/AppDelegate.m
+++ FeedbackDemo/FeedbackDemo/AppDelegate.m
@@ -34,12 +34,7 @@
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-	// Warn developer if API key or app ID are not set.
-	if ([[ATConnect sharedConnection].apiKey isEqualToString:@"ApptentiveApiKey"]) {
-		[[[UIAlertView alloc] initWithTitle:@"Please Set API Key" message:@"This demo app will not work properly until you set your API key in AppDelegate.m" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil] show];
-	} else if ([[ATConnect sharedConnection].appID isEqualToString:@"ExampleAppID"]) {
-		[[[UIAlertView alloc] initWithTitle:@"Please Set App ID" message:@"This demo app won't be able to show your app in the app store until you set your App ID in AppDelegate.m" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil] show];
-	}
+    [self showAPIKeyWarning];
 }
 
 - (BOOL)setupTestFlight {
@@ -56,6 +51,24 @@
     }
     
     return (testFlightAPIKey != nil);
+}
+
+- (void)showAPIKeyWarning {
+    if ([[ATConnect sharedConnection].apiKey isEqualToString:@"ApptentiveApiKey"]) {
+        NSLog(@"---");
+        NSLog(@"---");
+        NSLog(@"Please set Apptentive API Key! This demo app will not work properly until you set your API key in AppDelegate.m");
+        NSLog(@"---");
+        NSLog(@"---");
+    }
+    
+    if ([[ATConnect sharedConnection].appID isEqualToString:@"ExampleAppID"]) {
+        NSLog(@"---");
+        NSLog(@"---");
+        NSLog(@"Please Set App ID! This demo app won't be able to show your app in the app store until you set your App ID in AppDelegate.m");
+        NSLog(@"---");
+        NSLog(@"---");
+    }
 }
 
 @end

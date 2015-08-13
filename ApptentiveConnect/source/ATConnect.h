3abef062f96977c336c318a37dcda9443e0f5c42
Peter Kamb <peterkamb@gmail.com>
2015-08-13 15:52:59 -0700

    Update version number to 2.0.0 for Message Center release.

--- ApptentiveConnect/source/ATConnect.h
+++ ApptentiveConnect/source/ATConnect.h
@@ -13,7 +13,7 @@
 #import <Cocoa/Cocoa.h>
 #endif
 
-#define kATConnectVersionString @"1.7.3"
+#define kATConnectVersionString @"2.0.0"
 
 #if TARGET_OS_IPHONE
 #	define kATConnectPlatformString @"iOS"

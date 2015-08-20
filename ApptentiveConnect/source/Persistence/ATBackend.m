30d2d87b4b4f510a82fda307f8b75d91cd439549
Peter Kamb <peterkamb@gmail.com>
2015-08-19 23:26:41 -0700

    Remove metric when message sends CC 77caa38424aebe30d4deac0ab08b981435241c38

--- ApptentiveConnect/source/Persistence/ATBackend.m
+++ ApptentiveConnect/source/Persistence/ATBackend.m
@@ -305,9 +305,6 @@ static NSURLCache *imageCache = nil;
 		return NO;
 	}
 	
-#warning Invoke Event
-	//[[NSNotificationCenter defaultCenter] postNotificationName:ATMessageCenterDidSendNotification object:@{ATMessageCenterMessageNonceKey:message.pendingMessageID}];
-	
 	return [self sendMessage:message];
 }
 

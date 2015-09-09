983908459e98f7412f1ecb039eeb45de143182bc
Frank Schmitt <frank@apptentive.com>
2015-09-08 21:54:14 -0700

    Only set up required who card if email not present

--- ApptentiveConnect/source/Message Center/Controllers/ATMessageCenterViewController.m
+++ ApptentiveConnect/source/Message Center/Controllers/ATMessageCenterViewController.m
@@ -181,7 +181,7 @@ typedef NS_ENUM(NSInteger, ATMessageCenterState) {
 		self.profileView.skipButton.hidden = self.interaction.profileRequired;
 		[self validateWho:self];
 
-		if (self.interaction.profileRequired) {
+		if (self.interaction.profileRequired && [self shouldShowProfileViewBeforeComposing:YES]) {
 			self.profileView.skipButton.hidden = YES;
 			self.profileView.mode = ATMessageCenterProfileModeCompact;
 			

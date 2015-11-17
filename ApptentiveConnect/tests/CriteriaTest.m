168f4c8b5dc54e99950c05940820f3c9d9b8906e
Frank Schmitt <frank@apptentive.com>
2015-11-17 15:59:46 -0800

    [WIP] Add tests from Android

--- ApptentiveConnect/tests/CriteriaTest.m
+++ ApptentiveConnect/tests/CriteriaTest.m
@@ -8,6 +8,7 @@
 
 #import "CriteriaTest.h"
 #import "ATInteractionInvocation.h"
+#import "ATConnect.h"
 
 @implementation CriteriaTest
 
@@ -32,6 +33,66 @@
 
 		self.interaction = [ATInteractionInvocation invocationWithJSONDictionary:invocationDictionary];
 	}
+}
+
+@end
+
+
+@interface CornerCasesThatShouldBeFalse : CriteriaTest
+@end
+
+@implementation CornerCasesThatShouldBeFalse
+
+- (void)testCornerCasesThatShouldBeFalse {
+	XCTAssertFalse([self.interaction criteriaAreMet]);
+}
+
+@end
+
+
+@interface CornerCasesThatShouldBeTrue : CriteriaTest
+@end
+
+@implementation CornerCasesThatShouldBeTrue
+
+- (void)testCornerCasesThatShouldBeTrue {
+	XCTAssertTrue([self.interaction criteriaAreMet]);
+}
+
+@end
+
+
+@interface DefaultValues : CriteriaTest
+@end
+
+@implementation DefaultValues
+
+- (void)testDefaultValues {
+	XCTAssertTrue([self.interaction criteriaAreMet]);
+}
+
+@end
+
+
+@interface PredicateParsing : CriteriaTest
+@end
+
+@implementation PredicateParsing
+
+- (void)testPredicateParsing {
+	XCTAssertNotNil([self.interaction valueForKey:@"criteriaPredicate"]);
+}
+
+@end
+
+@interface OperatorContains : CriteriaTest
+@end
+
+@implementation OperatorContains
+
+- (void)testOperatorContains {
+	[ATConnect sharedConnection].personEmailAddress = @"test@example.com";
+	XCTAssertTrue([self.interaction criteriaAreMet]);
 }
 
 @end

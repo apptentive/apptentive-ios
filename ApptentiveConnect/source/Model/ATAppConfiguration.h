44d00a2519cc77eb101e8e47600f03ac70a89aa3
Frank Schmitt <frank@apptentive.com>
2016-01-21 17:42:16 -0800

    Move app configuration to custom object

---
+++ ApptentiveConnect/source/Model/ATAppConfiguration.h
@@ -0,0 +1,13 @@
+//
+//  ATAppConfiguration.h
+//  ApptentiveConnect
+//
+//  Created by Frank Schmitt on 1/21/16.
+//  Copyright © 2016 Apptentive, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATAppConfiguration : NSObject
+
+@end
---
+++ ApptentiveConnect/source/Model/ATAppConfiguration.m
@@ -0,0 +1,13 @@
+//
+//  ATAppConfiguration.m
+//  ApptentiveConnect
+//
+//  Created by Frank Schmitt on 1/21/16.
+//  Copyright © 2016 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATAppConfiguration.h"
+
+@implementation ATAppConfiguration
+
+@end

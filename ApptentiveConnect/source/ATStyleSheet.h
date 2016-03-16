7f1237690df16b73942dc529d527ade48622465d
Frank Schmitt <frank@apptentive.com>
2016-03-16 10:49:21 -0700

    WIP

---
+++ ApptentiveConnect/source/ATStyleSheet.h
@@ -0,0 +1,13 @@
+//
+//  ATStyleSheet.h
+//  ApptentiveConnect
+//
+//  Created by Frank Schmitt on 3/15/16.
+//  Copyright © 2016 Apptentive, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATStyleSheet : NSObject
+
+@end
---
+++ ApptentiveConnect/source/ATStyleSheet.m
@@ -0,0 +1,13 @@
+//
+//  ATStyleSheet.m
+//  ApptentiveConnect
+//
+//  Created by Frank Schmitt on 3/15/16.
+//  Copyright © 2016 Apptentive, Inc. All rights reserved.
+//
+
+#import "ATStyleSheet.h"
+
+@implementation ATStyleSheet
+
+@end

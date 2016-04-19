16ef07527d36480a586afcad96e958e192b91e3c
Frank Schmitt <frank@apptentive.com>
2016-04-19 15:28:41 -0700

    Update comment on placeholder and text input color keys

--- ApptentiveConnect/source/Apptentive.h
+++ ApptentiveConnect/source/Apptentive.h
@@ -640,10 +640,10 @@ extern NSString *const ApptentiveColorBackground;
 /// The background color for table- and collection views.
 extern NSString *const ApptentiveColorCollectionBackground;
 
-/// The background color for text input fields. Changing this may render placeholder invisible.
+/// The background color for text input fields.
 extern NSString *const ApptentiveColorTextInputBackground;
 
-/// The background color for text input fields. Changing this may render placeholder invisible.
+/// The color for text input placeholder text.
 extern NSString *const ApptentiveColorTextInputPlaceholder;
 
 /// The background color for message cells in Message Center.

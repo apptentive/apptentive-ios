c732c5884344b63c126f0c896f2f9a2b1095a57f
Frank Schmitt <frank@apptentive.com>
2016-07-07 18:01:46 -0700

    Bump version, podspec, changelog

--- ApptentiveConnect/source/Apptentive.h
+++ ApptentiveConnect/source/Apptentive.h
@@ -9,7 +9,7 @@
 
 #import <UIKit/UIKit.h>
 
-#define kApptentiveVersionString @"3.1.1"
+#define kApptentiveVersionString @"3.2.0"
 #define kApptentivePlatformString @"iOS"
 
 NS_ASSUME_NONNULL_BEGIN

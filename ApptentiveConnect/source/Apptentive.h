af05fc5b331f32223dd11b732fe54d3e4fdef332
Frank Schmitt <frank@apptentive.com>
2016-08-24 14:36:21 -0700

    Mark untyped custom data setters as unavailable in Swift

--- ApptentiveConnect/source/Apptentive.h
+++ ApptentiveConnect/source/Apptentive.h
@@ -404,7 +404,7 @@ NS_SWIFT_NAME(extendedData(itemID:name:category:price:quantity:currency:));
  @param object Custom data of type `NSDate`, `NSNumber`, or `NSString`.
  @param key A key to associate the data with.
  */
-- (void)addCustomPersonData:(NSObject<NSCoding> *)object withKey:(NSString *)key;
+- (void)addCustomPersonData:(NSObject<NSCoding> *)object withKey:(NSString *)key NS_SWIFT_UNAVAILABLE("Use the string, number, or boolean custom data setter");
 
 /**
  Adds custom data associated with the current device.
@@ -415,7 +415,7 @@ NS_SWIFT_NAME(extendedData(itemID:name:category:price:quantity:currency:));
  @param object Custom data of type `NSDate`, `NSNumber`, or `NSString`.
  @param key A key to associate the data with.
  */
-- (void)addCustomDeviceData:(NSObject<NSCoding> *)object withKey:(NSString *)key;
+- (void)addCustomDeviceData:(NSObject<NSCoding> *)object withKey:(NSString *)key NS_SWIFT_UNAVAILABLE("Use the string, number, or boolean custom data setter");
 
 /**
  Removes custom data associated with the current person.

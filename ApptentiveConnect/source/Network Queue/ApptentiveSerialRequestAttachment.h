da9f814fd54f90a522d364e98a1d02e35d8ab571
Frank Schmitt <frank@apptentive.com>
2017-01-19 09:45:03 -0800

    Fix inconsistent property specifier order

--- ApptentiveConnect/source/Network Queue/ApptentiveSerialRequestAttachment.h
+++ ApptentiveConnect/source/Network Queue/ApptentiveSerialRequestAttachment.h
@@ -10,12 +10,20 @@
 
 @class ApptentiveSerialRequest;
 
+
+/**
+ An `ApptentiveSerialRequestAttachment` represents an attachment to a message
+ sent from Message Center.
+
+ It includes the information needed to construct part of a multipart request
+ corresponding to the attachment.
+ */
 @interface ApptentiveSerialRequestAttachment : NSManagedObject
 
 @property (retain, nonatomic) NSString *mimeType;
 @property (retain, nonatomic) NSString *name;
 @property (retain, nonatomic) NSString *path;
-@property (nonatomic, retain) ApptentiveSerialRequest *request;
+@property (retain, nonatomic) ApptentiveSerialRequest *request;
 @property (readonly, retain, nonatomic) NSData *fileData;
 
 + (instancetype)queuedAttachmentWithName:(NSString *)name path:(NSString *)path MIMEType:(NSString *)mimeType inContext:(NSManagedObjectContext *)context;

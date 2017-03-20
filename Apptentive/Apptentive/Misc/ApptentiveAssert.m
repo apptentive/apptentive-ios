0cb7dcf38f2c2e6029373166f3955899c6a0c467
Alex Lementuev <a.lementuev@gmail.com>
2017-03-20 14:25:22 -0700

    Added Assertion stubs

---
+++ Apptentive/Apptentive/Misc/ApptentiveAssert.m
@@ -0,0 +1,13 @@
+//
+//  ApptentiveAssert.m
+//  Apptentive
+//
+//  Created by Alex Lementuev on 3/20/17.
+//  Copyright © 2017 Apptentive, Inc. All rights reserved.
+//
+
+#import "ApptentiveAssert.h"
+
+@implementation ApptentiveAssert
+
+@end

aecd94fdd0da4676a979243cce623d5a7d70f866
Alex Lementuev <a.lementuev@gmail.com>
2017-05-10 13:39:30 -0700

    Conversation migration testing progress
    Test Code Points Invokes

--- Apptentive/ApptentiveTests/ApptentiveConversationMigrationTests.m
+++ Apptentive/ApptentiveTests/ApptentiveConversationMigrationTests.m
@@ -16,6 +16,12 @@
 #import "ApptentiveCount.h"
 #import "ApptentiveAppDataContainer.h"
 
+static inline NSDate *dateFromString(NSString *date) {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"MM/dd/yyyy HH:mm:ss aa";
+    return [formatter dateFromString:date];
+}
+
 @interface ApptentiveConversationMigrationTests : XCTestCase
 
 @end
@@ -37,11 +43,19 @@
     
     ApptentiveConversation *conversation = [[ApptentiveConversation alloc] initAndMigrate];
     XCTAssertNotNil(conversation);
+    XCTAssertEqualObjects(conversation.token, @"1c496320bd0dbca0aad7e774f4eb3ec595f620e1df7a4afc9da37e5536bd5851");
+    XCTAssertEqualObjects(conversation.person.identifier, @"59124f8d09e3da650e000037");
+    XCTAssertEqualObjects(conversation.device.identifier, @"59124f8d09e3da650e000035");
 
-	XCTAssertNotNil(conversation.appRelease);
+	XCTAssertEqualObjects(conversation.appRelease.timeAtInstallTotal, dateFromString(@"01/01/2017 12:00:00 PM"));
+    XCTAssertEqualObjects(conversation.appRelease.timeAtInstallVersion, dateFromString(@"01/01/2017 12:00:00 PM"));
+    XCTAssertEqualObjects(conversation.appRelease.timeAtInstallBuild, dateFromString(@"01/01/2017 12:00:00 PM"));
+    
+    XCTAssertEqualObjects(conversation.device.customData, @{});
+    
 	XCTAssertNotNil(conversation.SDK);
 	XCTAssertNotNil(conversation.person);
-	XCTAssertNotNil(conversation.device);
+	
 	XCTAssertNotNil(conversation.engagement);
 
 	XCTAssertNotNil(conversation.appRelease.timeAtInstallTotal);
@@ -52,7 +66,10 @@
 
 	XCTAssertNotNil(conversation.device.customData);
 
-	XCTAssertEqual([conversation.engagement.codePoints[@"local#app#event_1"] totalCount], 1);
+	XCTAssertEqualObjects([conversation.engagement.codePoints[@"local#app#event_1"] lastInvoked], dateFromString(@"02/01/2017 12:00:00 PM"));
+    XCTAssertEqual([conversation.engagement.codePoints[@"local#app#event_1"] buildCount], 1);
+    XCTAssertEqual([conversation.engagement.codePoints[@"local#app#event_1"] versionCount], 2);
+    XCTAssertEqual([conversation.engagement.codePoints[@"local#app#event_1"] totalCount], 3);
 }
 
 @end

4dd10154f3883110540ecb4773713f3bd8548613
Frank Schmitt <frank@apptentive.com>
2017-08-10 13:52:05 -0700

    Continue with auth failure notification even w/o conversation ID

--- Apptentive/Apptentive/Network Queue/ApptentiveRequestOperation.m
+++ Apptentive/Apptentive/Network Queue/ApptentiveRequestOperation.m
@@ -231,12 +231,7 @@ NSErrorDomain const ApptentiveHTTPErrorDomain = @"com.apptentive.http";
 		return;
 	}
 
-	NSString *conversationIdentifier = self.request.conversationIdentifier;
-	ApptentiveAssertTrue(conversationIdentifier.length > 0, @"Conversation identifier is nil or empty");
-	if (conversationIdentifier.length == 0) {
-		return;
-	}
-
+	NSString *conversationIdentifier = self.request.conversationIdentifier ?: @"NO CONVERSATION";
 	NSString *errorType = ApptentiveDictionaryGetString(jsonObject, @"error_type") ?: @"UNKNOWN";
 	NSString *errorMessage = ApptentiveDictionaryGetString(jsonObject, @"error") ?: @"Unknown error";
 

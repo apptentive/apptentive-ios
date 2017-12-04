19d7ec7f827d6692251c1b7a6e0bf9077f1144f4
Alex Lementuev <a.lementuev@gmail.com>
2017-12-04 14:52:28 -0800

    Added a wrapper class for dispatch queues

---
+++ Apptentive/Apptentive/Misc/ApptentiveDispatchQueue.m
@@ -0,0 +1,58 @@
+//
+//  ApptentiveDispatchQueue.m
+//  Apptentive
+//
+//  Created by Alex Lementuev on 12/4/17.
+//  Copyright © 2017 Apptentive, Inc. All rights reserved.
+//
+
+#import "ApptentiveDispatchQueue.h"
+
+#import "ApptentiveAssert.h"
+#import "ApptentiveDefines.h"
+#import "ApptentiveGCDDispatchQueue.h"
+
+static ApptentiveDispatchQueue * _mainQueue;
+static ApptentiveDispatchQueue * _backgroundQueue;
+
+@implementation ApptentiveDispatchQueue
+
++ (void)initialize {
+	if ([self class] == [ApptentiveDispatchQueue class]) {
+		_mainQueue = [[ApptentiveGCDDispatchQueue alloc] initWithQueue:dispatch_get_main_queue()];
+		_backgroundQueue = [[ApptentiveGCDDispatchQueue alloc] initWithQueue:dispatch_queue_create("Apptentive Background Queue", DISPATCH_QUEUE_CONCURRENT)];
+	}
+}
+
++ (instancetype)main {
+	return _mainQueue;
+}
+
++ (instancetype)background {
+	return _backgroundQueue;
+}
+
++ (instancetype)createQueueWithName:(NSString *)name type:(ApptentiveDispatchQueueConcurrencyType)type {
+	if (type == ApptentiveDispatchQueueConcurrencyTypeSerial) {
+		const char *label = name.UTF8String;
+		return [[ApptentiveGCDDispatchQueue alloc] initWithQueue:dispatch_queue_create(label, DISPATCH_QUEUE_SERIAL)];
+	}
+	
+	if (type == ApptentiveDispatchQueueConcurrencyTypeConcurrent) {
+		const char *label = name.UTF8String;
+		return [[ApptentiveGCDDispatchQueue alloc] initWithQueue:dispatch_queue_create(label, DISPATCH_QUEUE_CONCURRENT)];
+	}
+	
+	ApptentiveAssertFail(@"Unexpected concurrency type for queue '%@': %ld", name, type);
+	return nil;
+}
+
+- (void)dispatchAsync:(void (^)(void))task {
+	[self dispatchAsync:task afterDelay:0.0];
+}
+
+- (void)dispatchAsync:(void (^)(void))task afterDelay:(NSTimeInterval)delay {
+	APPTENTIVE_ABSTRACT_METHOD_CALLED
+}
+
+@end
---
+++ Apptentive/Apptentive/Misc/ApptentiveGCDDispatchQueue.h
@@ -0,0 +1,19 @@
+//
+//  ApptentiveGCDDispatchQueue.h
+//  Apptentive
+//
+//  Created by Alex Lementuev on 12/4/17.
+//  Copyright © 2017 Apptentive, Inc. All rights reserved.
+//
+
+#import "ApptentiveDispatchQueue.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ApptentiveGCDDispatchQueue : ApptentiveDispatchQueue
+
+- (instancetype)initWithQueue:(dispatch_queue_t)queue;
+
+@end
+
+NS_ASSUME_NONNULL_END

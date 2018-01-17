c3c3b729bae7bf97c041a9c57b91cfa8c92e8b4a
Frank Schmitt <frank@apptentive.com>
2018-01-16 17:10:46 -0800

    Remove padding above submit button when keyboard shrinks the available area

--- Apptentive/Apptentive/Surveys/View Controllers/ApptentiveSurveyCollectionViewLayout.h
+++ Apptentive/Apptentive/Surveys/View Controllers/ApptentiveSurveyCollectionViewLayout.h
@@ -12,6 +12,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 
 @interface ApptentiveSurveyCollectionViewLayout : UICollectionViewFlowLayout
+
+@property (assign, nonatomic) BOOL shouldExpand;
+
 @end
 
 NS_ASSUME_NONNULL_END
